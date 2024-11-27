b84f1e243d14f5ae924d6a2a578bb56238ce1dac
idasegolsson <seid24yr@student.ju.se>
2024-11-27 09:41:59 +0100

    first update

--- game.js
+++ game.js
@@ -1,7 +1,37 @@
+let state = "start";
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(700, 700);
 }
 
-function draw() {
-  background(255, 140, 0);
+function draw() {}
+
+function startScreen() {
+  //button
+  //background of savannah with lion from illustartor
+  //meerkat
+}
+
+function gameScreen() {}
+
+function winScreen() {
+  //meerkat holding nut
+}
+
+function loseScreen() {
+  //sad meerkat
+}
+
+function mouseClicked() {
+  if (state === "start") {
+    state = "game";
+  } else if (state === "lost") {
+    state = "start";
+    //velocity = 0; characterY = -400; from alien game
+  } else if (state === "win") {
+    state = "start";
+    //velocity = 0; characterY = -400; from alien game
+
+    // we have to fix so it is adapted to this game, if meerkat touches lion he loses
+  }
 }
